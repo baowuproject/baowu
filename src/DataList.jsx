6774a3c5a32fb673c6649f19102b4db580118313
renhaoran <RHRxjtu@outlook.com>
2019-06-10 13:51:12 +0800

    add left

---
+++ src/DataList.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import { List, Typography } from 'antd';
+
+class DataList extends Component{
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default DataList
